aef9534d356f5be86eb6983241b22e5115f06acb
ioana.girba <>
2024-04-04 15:01:31 +0300

    Create auth context

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,17 +1,20 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 
 import AuthContent from "../components/Auth/AuthContent";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 import { login } from "../util/auth";
 import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
 
 function LoginScreen() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const authCtx = useContext(AuthContext);
 
   const loginHandler = async ({ email, password }) => {
     setIsAuthenticated(true);
     try {
-      await login(email, password);
+      const token = await login(email, password);
+      authCtx.authenticate(token);
     } catch {
       Alert.alert("Authentication failed", "Could not log you in");
     }
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,16 +1,19 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import AuthContent from "../components/Auth/AuthContent";
 import { createUser } from "../util/auth";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { AuthContext } from "../store/auth-context";
 
 function SignupScreen() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const authCtx = useContext(AuthContext);
 
   const signupHandler = async ({ email, password }) => {
     setIsAuthenticated(true);
     try {
-      await createUser(email, password);
+      const token = await createUser(email, password);
+      authCtx.authenticate(token);
     } catch {
       Alert.alert("Authentication failed", "Could not create user");
     }
