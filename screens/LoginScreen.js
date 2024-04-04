8de9a384dce00f1ba32da2d595008a4b828a4c23
ioana.girba <>
2024-04-04 14:24:48 +0300

    Handle errors

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -3,12 +3,18 @@ import { useState } from "react";
 import AuthContent from "../components/Auth/AuthContent";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
 import { login } from "../util/auth";
+import { Alert } from "react-native";
 
 function LoginScreen() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   const loginHandler = async ({ email, password }) => {
     setIsAuthenticated(true);
-    await login(email, password);
+    try {
+      await login(email, password);
+    } catch {
+      Alert.alert("Authentication failed", "Could not log you in");
+    }
     setIsAuthenticated(false);
   };
 
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -6,9 +6,14 @@ import LoadingOverlay from "../components/ui/LoadingOverlay";
 
 function SignupScreen() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   const signupHandler = async ({ email, password }) => {
     setIsAuthenticated(true);
-    await createUser(email, password);
+    try {
+      await createUser(email, password);
+    } catch {
+      Alert.alert("Authentication failed", "Could not create user");
+    }
     setIsAuthenticated(false);
   };
 
